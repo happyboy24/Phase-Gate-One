48bcea64f0ce8a79351a07dc25bc73db792d25bc
happyboy24 <cejika134@gmail.com>
2025-10-28 09:46:18 +0100

    first commit

--- Java/DAY ONE/SimpleArithmeticAppp.java
+++ Java/DAY ONE/SimpleArithmeticAppp.java
@@ -8,21 +8,21 @@ public class SimpleArithmeticAppp {
 	Scanner input = new Scanner(System.in);
 	Random random = new Random();
 
-	int GottenAnswer = 0;
+	int ConfirmedAnswer = 0;
 
 	System.out.println("I WELCOME YOU TO SIMPLE ARITHMETIC APP");
 
 	for(int count = 1; count <= 10; count++){
 	int number1 = random.nextInt(50) + 1;
 	int number2 = random.nextInt(number1) + 1;
-	int gotten = number1 - number2;
+	int correct = number1 - number2;
 	System.out.println(count + number1 + " - " + number2 + " = ");
 	System.out.println("Enter your answer:");
-	int userinput = input.nextInt();
+	int usersinput = input.nextInt();
 
-if (userinput != gotten){
-	GottenAnswer += gotten;
-	GottenAnswer++;
+if (usersinput == correct){
+	ConfirmedAnswer += correct;
+	ConfirmedAnswer++;
 System.out.println("Correct Answer");
 }
 else {
@@ -30,9 +30,9 @@ System.out.println("Incorrect Answer");
 System.out.println(count + number1 + " - " + number2 + " = ");
 }
 	
-//System.out.println("Total score: " + GottenAnswer );
+//System.out.println("Total score: " + ConfirmedAnswer );
 }
-System.out.println("Total score: " + GottenAnswer );
+System.out.println("Total score: " + ConfirmedAnswer );
 
 
 }
